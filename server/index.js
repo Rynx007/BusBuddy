3cb9ff8b52aba617915384a5e9719fc81bacc6f0
Rynx007 <rynx007@gmail.com>
2024-09-15 22:47:47 +0545

    Initiated the server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-const express = required("express");
+const express = require("express");
+const app = express();
+
+
+// Response in server
+app.get('/', (req, res) => {
+    res.send('Hello Wordl');
+});
+
+
+// Start the server
+app.listen(3002, () => {
+    console.log("Server is running in 3002 port");
+});
